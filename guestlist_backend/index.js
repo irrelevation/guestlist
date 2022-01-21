03f42b4a0813491bff3ec7dbf8dbe10fa6b41cdf
Lukas Zöllner <lks.zoellner@gmail.com>
2022-01-21 17:51:31 +0100

    add basic error handler, add mongo client, add environment variables

--- guestlist_backend/index.js
+++ guestlist_backend/index.js
@@ -1,6 +1,8 @@
-import { logger } from "./logger/index.js";
+import "dotenv/config";
+import { logger } from "./src/logger/index.js";
 import { app } from "./src/app.js";
 
+console.log(process.env.PORT);
 const PORT = process.env.port ?? 3000;
 
 app.listen(PORT, () => logger.info(`listening on port ${PORT}`));
--- guestlist_backend/src/app.js
+++ guestlist_backend/src/app.js
@@ -1,17 +1,27 @@
 import express from "express";
+import { errorHandler, routeNotFound } from "./middleware/errorMiddleware.js";
+import { loggerMiddleware } from "./middleware/loggerMiddleware.js";
 import { eventRouter } from "./routes/eventRoutes.js";
 import { userRouter } from "./routes/userRoutes.js";
+import cors from "cors";
+import mongoose from "mongoose";
+import { logger } from "./logger/index.js";
+
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.hcyd9.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
+mongoose
+  .connect(MONGO_URL)
+  .then(() => logger.info("Successfully connected to DB"))
+  .catch((e) => logger.error(e));
 
 export const app = express();
 const API_URL_PREFIX = "/api/v1";
 
-app.get(`${API_URL_PREFIX}/`, (req, res) => {
-  return res.status(200).send({
-    message: "API works",
-  });
-});
-
+app.use(loggerMiddleware);
 app.use(express.json());
+app.use(cors());
 
 app.use(`${API_URL_PREFIX}/events`, eventRouter);
 app.use(`${API_URL_PREFIX}/users`, userRouter);
+
+app.use(routeNotFound);
+app.use(errorHandler);
